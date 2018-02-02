bc25a1661d5998ad29124eb8cfd8b7dcac5f57a7
krinistof <kristof0116@gmail.com>
2018-02-02 06:08:36 +0100

    Katyubol való gyorsulás megcsinálva

--- core/src/com/mygdx/game/CarActor.java
+++ core/src/com/mygdx/game/CarActor.java
@@ -226,7 +226,7 @@ public class CarActor extends Vehicle {
         super.act(delta);
         //System.out.println("getY() = " + getY());
         if(wantedSpeed > currentSpeed)
-            currentSpeed *= 1.1;
+            currentSpeed *= 1.001;
         if(mehet) {
             if (mehetBalraAlap)
                 if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.getAccelerometerY() < -3) {
